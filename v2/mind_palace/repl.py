fd427753990d60574db6d76cd5ce7b92363506c7
Paul Lam <paul@quantisan.com>
2023-09-04 08:34:51 +0900

    refactored out a index_documents()

--- v2/mind_palace/repl.py
+++ v2/mind_palace/repl.py
@@ -10,6 +10,7 @@ def time_function(func):
 
 
 def load_documents():
+    # TODO: refactor this step out (so I'm keeping it self-contained with imports for now)
     from llama_index import SimpleDirectoryReader
 
     required_exts = [".pdf"]
@@ -24,25 +25,30 @@ docs, elapsed_time = time_function(load_documents)
 print(f"Elapsed time {elapsed_time:.1f} seconds: Loaded {len(docs)} total pages (aka Documents) from {len(set([doc.metadata['file_name'] for doc in docs]))} PDFs")
 
 # Index Construction
-from llama_index.llms import OpenAI
-from llama_index.embeddings import OpenAIEmbedding
+def index_documents(documents):
+    # TODO: refactor this step out (so I'm keeping it self-contained with imports for now)
+    from llama_index.llms import OpenAI
 
-from llama_index import ServiceContext
-from llama_index import VectorStoreIndex
+    from llama_index import ServiceContext
+    from llama_index import VectorStoreIndex
 
-service_context = ServiceContext.from_defaults(
-    llm=OpenAI(model="gpt-3.5-turbo"),
-    embed_model="local"
-    # Q: how much does a better embedding model help?
-)
+    service_context = ServiceContext.from_defaults(
+        llm=OpenAI(model="gpt-3.5-turbo"),
+        embed_model="local"
+        # Q: how much does a better embedding model help?
+    )
 
-# add docs to a VectorStoreIndex one at a time to avoid OpenAI rate limits
-# note that we reverted to not using OpenAI for embedding for now because it's slow
-print(f"Adding {docs[0].metadata}")
-index = VectorStoreIndex.from_documents(docs[:1], service_context=service_context)
+    # add docs to a VectorStoreIndex one at a time to avoid OpenAI rate limits
+    # note that we reverted to not using OpenAI for embedding for now because it's slow
+    print(f"Adding {docs[0].metadata}")
+    index = VectorStoreIndex.from_documents(documents[:1], service_context=service_context)
 
-for doc in docs[1:]:
-    print(f"Adding {doc.metadata}")
-    index.insert(doc)
+    for doc in documents[1:]:
+        print(f"Adding {doc.metadata}")
+        index.insert(doc)
 
-print(f"Successfully indexed {len(docs)} Documents")
+    return index
+
+index, elapsed_time = time_function(lambda: index_documents(docs))
+print(f"Elapsed time {elapsed_time:.1f} seconds: Indexed {len(docs)} total pages (aka Documents) from {len(set([doc.metadata['file_name'] for doc in docs]))} PDFs")
+
