aa00be8e072691ea71f519e49d6cffc27d4c5e1d
Paul Lam <paul@quantisan.com>
2023-09-04 08:28:28 +0900

    add a time_function() and add some step by step logging

--- v2/mind_palace/repl.py
+++ v2/mind_palace/repl.py
@@ -1,16 +1,27 @@
-# Load in documents
+import time
 
-from llama_index import SimpleDirectoryReader
 
-required_exts = [".pdf"]
+def time_function(func):
+    start_time = time.time()
+    result = func()
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    return result, elapsed_time
 
-docs = SimpleDirectoryReader(
-    input_dir="./resources/pdfs/12-pdfs-from-steve-aug-22", required_exts=required_exts
-).load_data()
-# Q: we could use a better text extractor supporting section-aware extraction
 
-print(f"Loaded {len(docs)} docs")
-# each page of a PDF becomes a separate doc object
+def load_documents():
+    from llama_index import SimpleDirectoryReader
+
+    required_exts = [".pdf"]
+
+    # Q: we could use a better text extractor supporting section-aware extraction
+    return SimpleDirectoryReader(
+        input_dir="./resources/pdfs/12-pdfs-from-steve-aug-22", required_exts=required_exts
+    ).load_data()
+
+
+docs, elapsed_time = time_function(load_documents)
+print(f"Elapsed time {elapsed_time:.1f} seconds: Loaded {len(docs)} total pages (aka Documents) from {len(set([doc.metadata['file_name'] for doc in docs]))} PDFs")
 
 # Index Construction
 from llama_index.llms import OpenAI
@@ -26,8 +37,12 @@ service_context = ServiceContext.from_defaults(
 )
 
 # add docs to a VectorStoreIndex one at a time to avoid OpenAI rate limits
+# note that we reverted to not using OpenAI for embedding for now because it's slow
+print(f"Adding {docs[0].metadata}")
 index = VectorStoreIndex.from_documents(docs[:1], service_context=service_context)
 
 for doc in docs[1:]:
     print(f"Adding {doc.metadata}")
     index.insert(doc)
+
+print(f"Successfully indexed {len(docs)} Documents")
