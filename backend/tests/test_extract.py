51a0f56fcb9179a78eb5ee6dc723c749dfa39d8b
Paul Lam <paul@quantisan.com>
2023-09-13 08:03:44 +0900

    add test_load_tei_xml

--- backend/tests/test_extract.py
+++ backend/tests/test_extract.py
@@ -1,8 +1,20 @@
 from .context import extract
 
+XML_PATH = "./resources/pdfs/12-pdfs-from-steve-aug-22/xml/"
+
+
+def test_load_tei_xml():
+    nodes = extract._load_tei_xml(
+        XML_PATH
+        + "2010_PhysRevLett_Pulsating Tandem Microbubble for Localized and Directional Single-Cell Membrane Poration.pdf.tei.xml"
+    )
+    assert isinstance(nodes, list)
+    for node in nodes:
+        assert isinstance(node, extract.TextNode)
+
 
 def test_seed_nodes():
-    input_dir = "./resources/pdfs/12-pdfs-from-steve-aug-22/xml"
+    input_dir = XML_PATH
     nodes = extract.seed_nodes(input_dir)
     assert isinstance(nodes, list)
     for node in nodes:
