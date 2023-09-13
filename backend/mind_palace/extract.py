4097d81e2bde918e400613511752e0229edfe004
Paul Lam <paul@quantisan.com>
2023-09-13 11:08:15 +0900

    remove extract.load_pdfs()

--- backend/mind_palace/extract.py
+++ backend/mind_palace/extract.py
@@ -5,31 +5,6 @@ from llama_index.schema import TextNode
 import docs
 
 
-def load_pdfs():
-    required_exts = [".pdf"]
-
-    # Q: we could use a better text extractor supporting section-aware extraction
-    return li.SimpleDirectoryReader(
-        input_dir="./resources/pdfs/12-pdfs-from-steve-aug-22",
-        required_exts=required_exts,
-    ).load_data()
-
-
-def nodes(documents, service_context=li.ServiceContext.from_defaults()):
-    return service_context.node_parser.get_nodes_from_documents(
-        documents, show_progress=True
-    )
-
-
-# a Node from load_pdfs standard pipeline
-#
-# >>> nodes[0].__dict__.keys()
-# dict_keys(['id_', 'embedding', 'metadata', 'excluded_embed_metadata_keys',
-# 'excluded_llm_metadata_keys', 'relationships', 'hash', 'text',
-# 'start_char_idx', 'end_char_idx', 'text_template', 'metadata_template',
-# 'metadata_seperator'])
-
-
 def _gen_document_dict(file_path) -> dict[str, TextNode]:
     xml = docs.load_tei_xml(file_path)
     doi = xml.header.doi
