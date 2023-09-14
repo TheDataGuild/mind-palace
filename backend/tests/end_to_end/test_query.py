b869e3249b317d700acb4dd900e44da9dac7daf9
Paul Lam <paul@quantisan.com>
2023-09-14 13:53:09 +0900

    skip test_query() actual query check, left as a TODO

--- backend/tests/end_to_end/test_query.py
+++ backend/tests/end_to_end/test_query.py
@@ -14,22 +14,16 @@ def persist_index(vector_index):
 
 
 def test_query():
-    storage_context = li.StorageContext.from_defaults(persist_dir="./tests/end_to_end/llama_stores/12-pdfs-from-steve-aug-22/")
+    storage_context = li.StorageContext.from_defaults(
+        persist_dir="./tests/end_to_end/llama_stores/12-pdfs-from-steve-aug-22/"
+    )
     vector_index = li.load_index_from_storage(storage_context)
     assert vector_index is not None
 
-    # Querying
-    # query_engine = vector_index.as_query_engine()
+    # Query
+    query_engine = vector_index.as_query_engine()
+    assert query_engine is not None
 
-# This query calls out to OppenAI
-# response = query_engine.query("What interaction is investigated?")
-# >>> print(response)
-# The interaction of laser-generated tandem microbubble with single cells is investigated.
-#
-# >>> query_engine.query("What is the significant finding in this investigation?")
-# The significant finding in this investigation is that the interaction of
-# laser-generated tandem microbubbles with single cells can lead to the
-# formation of alternating, directional microjets and vortices. This
-# interaction can also result in localized and directional membrane poration,
-# which is absent when using a single oscillating microbubble of comparable
-# size and at the same stand-off distance.
+    # TODO:
+    # response = query_engine.query("What interaction is investigated?")
+    # assert response.get_response()
