9ffa00b15d634d7c3d55dae3ca47bc20f2bc2362
Paul Lam <paul@quantisan.com>
2023-09-26 10:29:10 +0900

    move index tests into test_index.py

--- tests/unit/test_query.py
+++ tests/unit/test_query.py
@@ -1,22 +1,5 @@
-from tests.context import extract, index
 import llama_index as li
 import pytest
-
-
-def bootstrap_index(xml_dir):
-    nodes = extract.seed_nodes(xml_dir)
-    return index.index_nodes(nodes)
-
-
-@pytest.mark.skip(reason="This test takes a minute to run")
-def test_bootstrap_index():
-    xml_dir = "./resources/xmls/12-pdfs-from-steve-aug-22/"
-    vector_index = bootstrap_index(xml_dir)
-    assert vector_index is not None
-
-
-def persist_index(vector_index):
-    vector_index.storage_context.persist()
 
 
 @pytest.mark.skip(reason="This cost money to run")
